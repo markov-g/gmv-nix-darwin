54cbb296c1f0b70b019a69c945d69984341ff3b0
Georgi Markov <g.markov@icloud.com>
2026-03-11 16:46:47 -0400

    add a backup extension so Home Manager moves conflicting files aside automatically

--- flake.nix
+++ flake.nix
@@ -80,6 +80,7 @@
           ({ ... }: {
             home-manager.useGlobalPkgs   = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-backup";
             home-manager.extraSpecialArgs = { inherit host user inputs; };
             home-manager.sharedModules   = [ inputs.sops-nix.homeManagerModules.sops ];
             home-manager.users.${user}   = import ./modules/home.nix;
